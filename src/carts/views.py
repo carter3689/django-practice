932a5ce6b0a22077b1414c984613c629299a6d40
Joel Carter <carter3689@gmail.com>
2017-08-16 23:57:00 -0500

    Working Cart example that takes in URL values and stores them to the database.

--- src/carts/views.py
+++ src/carts/views.py
@@ -11,11 +11,16 @@ class CartView(View):
 
     def get(self,request,*args,**kwargs):
         item_id = request.GET.get("item")
+        delete_item = request.GET.get("delete")
         if item_id:
             item_instance = get_object_or_404(Variation,id = item_id)
             qty = request.GET.get("qty")
             cart = Cart.objects.all()[0]
             cart_item = CartItem.objects.get_or_create(cart=cart, item = item_instance)[0]
-            cart_item.quantity = 20
-            print(cart_item)
+            if delete_item:
+                cart_item.delete()
+            else:
+                cart_item.quantity = qty
+                cart_item.save()
+                print(cart_item)
         return HttpResponseRedirect("/")
